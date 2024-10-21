7ef1af166f819f9ca485825341a28831b9024c93
Marek Wieiwórka <marek.wiewiorka@gmail.com>
2024-10-21 20:35:48 +0200

    Notebooks restructure

--- notebooks/cybersecurity/sms_classifier.py
+++ notebooks/cybersecurity/sms_classifier.py
@@ -1,3 +1,4 @@
+import dspy
 from pydantic import BaseModel, Field
 
 from enum import Enum
@@ -15,3 +16,26 @@ class Input(BaseModel):
 
 class Output(BaseModel):
     label: Label = Field(description="The predicted label for the SMS text")
+
+
+class SMSClassifierSignature(dspy.Signature):
+    """
+    Given an SMS text, predict whether it is ham, spam, or smishing.
+    Output only the predicted label.
+    """
+
+    input: Input = dspy.InputField()
+    output: Output = dspy.OutputField()
+
+
+class SMSClassifier(dspy.Module):
+    def __init__(self, lm):
+        self.lm = lm
+        super().__init__()
+        dspy.configure(lm=lm)
+        self.generate_answer = dspy.TypedPredictor(
+            SMSClassifierSignature, max_retries=1
+        )
+
+    def forward(self, input):
+        return self.generate_answer(input=input)
